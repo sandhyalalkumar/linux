3b317875ab5831f278081f7caa4cb98a4a402927
sandhyalalkumar <sandhyalalkumar@gmail.com>
2015-12-19 11:15:53 +0530

    New commands added

---
+++ commands/hpcc_ecl.py
@@ -0,0 +1,8 @@
+eclipse
+=======
+
+#Failed to start web browser - see error log - but where!
+#Is this a Linux based machine?
+#Assuming it is (and your localhost ECL Playground comment), you may need to install libwebkitgtk:
+
+$ sudo apt-get install libwebkitgtk-1.0-0
